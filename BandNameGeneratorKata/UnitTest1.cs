20fc662bffaf83cab7408326e6ce1c1b5e8e3ff2
katrina <inlove0131@hotmail.com>
2017-09-25 19:09:29 +0800

    https://github.com/katrinaliao/BandNameGeneratorKata.git

--- BandNameGeneratorKata/UnitTest1.cs
+++ BandNameGeneratorKata/UnitTest1.cs
@@ -1,14 +1,33 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace BandNameGeneratorKata
+namespace UnitTestProject1
 {
     [TestClass]
-    public class UnitTest1
+    public class BandNameTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Test()
         {
+            Assert.AreEqual("The Knife", BandName.Generate("knife"));
+            Assert.AreEqual("Tartart", BandName.Generate("tart"));
+            Assert.AreEqual("Sandlesandles", BandName.Generate("sandles"));
+            Assert.AreEqual("The Bed", BandName.Generate("bed"));
+        }
+
+        public class BandName
+        {
+            public static string Generate(string s)
+            {
+                var startIndex = s.Substring(0, 1);
+                var endIndex = s.Substring(s.Length - 1, 1);
+
+                if (startIndex == endIndex)
+                    return char.ToUpper(s[0]) + s.Substring(1) + s.TrimStart(startIndex.ToCharArray());
+
+                else
+                    return "The " + char.ToUpper(s[0]) + s.Substring(1);
+            }
         }
     }
 }
